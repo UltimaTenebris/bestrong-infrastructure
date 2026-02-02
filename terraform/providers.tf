de7c7d3e9abdb74bbb25975411eb5b2ca034ca75
Grenka <grenka1313@gmail.com>
2026-02-02 19:16:10 +0200

    app + cicd pipeline + helm charts

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -22,8 +22,8 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
-# resource "azurerm_role_assignment" "acr_pull" {
-#   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-#   role_definition_name = "AcrPull"
-#   scope                = azurerm_container_registry.acr.id
-# }
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
