8376cd836573b09c7729486d6fc8eeb1e6dd3e7c
Grenka <grenka1313@gmail.com>
2026-02-02 19:47:20 +0200

    -docker/ temp test cicd

--- app/Program.cs
+++ app/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // ===== BLUE / GREEN MARKER =====
-const string APP_VERSION = "1.0.0";
+const string APP_VERSION = "1.0.1";
 // ===============================
 
 // Add services to the container.
