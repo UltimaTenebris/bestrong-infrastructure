3fee59617bdb4af0d6625e65975c15a61825a985
pofany <148377278+pofany@users.noreply.github.com>
2026-02-04 17:25:26 +0200

    Remote Backend tf.state is now in Azure blob storage

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -27,3 +27,20 @@ resource "azurerm_role_assignment" "acr_pull" {
   role_definition_name = "AcrPull"
   scope                = azurerm_container_registry.acr.id
 }
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "bestrongtfstate123"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  min_tls_version          = "TLS1_2"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
