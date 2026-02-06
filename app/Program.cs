25702e17a5a5f19446de2b76f92fe27adccb1712
Grenka <grenka1313@gmail.com>
2026-02-06 14:03:44 +0200

    Added postgres in helm

--- app/Program.cs
+++ app/Program.cs
@@ -10,7 +10,20 @@ var APP_VERSION = Environment.GetEnvironmentVariable("APP_VERSION") ?? "1.1.0";
 // ===============================
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var baseConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (string.IsNullOrWhiteSpace(baseConnectionString))
+{
+    throw new InvalidOperationException("Connection string 'DefaultConnection' was not found.");
+}
+
+var pgPassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+if (string.IsNullOrWhiteSpace(pgPassword))
+{
+    throw new InvalidOperationException("POSTGRES_PASSWORD env var not set");
+}
+
+var connectionString = $"{baseConnectionString};Password={pgPassword}";
+
 if (string.IsNullOrWhiteSpace(connectionString))
 {
     throw new InvalidOperationException("Connection string 'DefaultConnection' was not found.");
